9bff5779cdc417168896dafeaf8c0caf0565ab11
Daniel Jacob Behnke <djbehnke@pop-os.localdomain>
2019-10-10 18:33:33 -0400

    Added classes for client side functionality

---
+++ PublishSubscribe/pubsub/PublisherNetWorkHandler.java
@@ -0,0 +1,25 @@
+package pubsub;
+
+public class PublisherNetworkHandler implements Runnable{
+
+    static final int PORT = 60666;
+
+    public class PublisherNetworkHandler{
+
+    }
+
+    public void run(){
+        try{
+
+            while(true){
+
+            }
+        }catch( Exception e ){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
---
+++ PublishSubscribe/runBroker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+java pubsub/Broker
