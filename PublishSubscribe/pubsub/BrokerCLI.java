e378717b0f90751e6eba43f319749590da1c36fa
Daniel Jacob Behnke <djbehnke.mail@gmail.com>
2019-10-15 14:11:11 -0400

    Added basic command line reading to BrokerCLI

--- PublishSubscribe/pubsub/BrokerCLI.java
+++ PublishSubscribe/pubsub/BrokerCLI.java
@@ -1,9 +1,10 @@
 /*
  * The BrokerCLI listens to stdin and is used to monitor the broker
  */
-
-
 package pubsub;
+
+
+import java.util.Scanner;
 
 public class BrokerCLI implements Runnable{
 
@@ -14,5 +15,11 @@ public class BrokerCLI implements Runnable{
     }
 
     public void run(){
+        Scanner scanner = new Scanner( System.in );
+        while(true){
+            System.out.print("> ");
+            
+            String userInput = scanner.next();
+        }
     }
 }
