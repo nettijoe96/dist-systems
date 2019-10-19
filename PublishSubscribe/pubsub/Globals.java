f6ba1ad14f62d60ec8a733f806e43c88129b943f
Daniel Jacob Behnke <djbehnke@pop-os.localdomain>
2019-10-18 20:05:26 -0400

    Added more functionality to the Broker CLI. Now can get help, list topics, and exit. It should be fairly safe from unexpected input.

--- PublishSubscribe/pubsub/Globals.java
+++ PublishSubscribe/pubsub/Globals.java
@@ -9,7 +9,7 @@ public class Globals {
 
     static final int serverConnectionBacklog = 100;
 
-    // Constants denoting certain types
+    // Constants denoting certain types (also used for cli commands)
     static final String ADVERTISE = "ADVERTISE";
     static final String CONNECT = "CONNECT";
     static final String PUBLISH = "PUBLISH";
@@ -17,8 +17,15 @@ public class Globals {
     static final String SUBSCRIBE = "SUBSCRIBE";
     static final String UNSUBSCRIBE = "UNSUBSCRIBE";
 
-    // CLI input to exit
-    static final String EXITLINE = "exit()";
+    // CLI input 
+    static final String EXIT_COMMAND = "EXIT";
+    static final String LIST_TOPICS_COMMAND = "TOPICS";
+    static final String HELP_COMMAND = "HELP";
+
+    static final String BROKER_HELP = "The following commands are available:\n" +
+        EXIT_COMMAND + "\n" +
+        LIST_TOPICS_COMMAND + "\n" +
+        HELP_COMMAND + "\n";
 
 
 }
