c8bec5255e9fff94c402814f5a464d844587761b
Daniel Jacob Behnke <djbehnke.mail@gmail.com>
2019-10-28 13:14:54 -0400

    Got AnchorNode to compile (no functionality). Basically just trying to get RMI to work.

--- Chord/src/AnchorNode.java
+++ Chord/src/AnchorNode.java
@@ -1,3 +1,9 @@
+import java.rmi.server.*;
+import java.rmi.*;
+
 public class AnchorNode extends UnicastRemoteObject implements AnchorNodeStub{
+
+    public AnchorNode() throws RemoteException{
+    }
 
 }
