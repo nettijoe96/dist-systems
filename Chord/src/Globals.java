4f40bb4222f257feb88d522163ffc5f1f8c4a111
Daniel Jacob Behnke <djbehnke.mail@gmail.com>
2019-10-31 13:40:13 -0400

    Added ring space size gloabal constant

--- Chord/src/Globals.java
+++ Chord/src/Globals.java
@@ -4,5 +4,6 @@ public class Globals {
 
     public static final int PORT = 60667;
     public static final String BROKER_IP = "127.17.0.1";      
+    public static final int RING_SPACE_SIZE = 16;
 
 }
