85850e7606d5c4ed17094839cdeab9f1b5e93ee5
Daniel Jacob Behnke <djbehnke.mail@gmail.com>
2019-11-01 13:23:16 -0400

    Added Dockerfile. Run runClient.sh, then once in the container go ahead and java src.ClientNodeLauncher [uuid] and it will boot up. Not sure that the nodes can communicate with each other right now though.

--- Chord/src/ClientNodeLauncher.java
+++ Chord/src/ClientNodeLauncher.java
@@ -2,7 +2,7 @@ package src;
 
 public class ClientNodeLauncher{
     public static void main( String[] arg ){
-        ClientNode node = new ClientNode( Integer.parseInt( arg[1] ) );
+        ClientNode node = new ClientNode( Integer.parseInt( arg[0] ) );
         Thread nodeThread = new Thread( node );
         nodeThread.start();
 
