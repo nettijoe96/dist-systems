eb572a4ff3e281b5c138e33f91a2c5a997653752
Daniel Jacob Behnke <djbehnke@pop-os.localdomain>
2019-11-05 21:30:31 -0500

    Removed sudo from run scripts

--- chord/runAnchor.sh
+++ chord/runAnchor.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 anchorIP="172.10.0.2"
-sudo docker run -it --net chord-20-net --ip $anchorIP chord-20:anchor java src.Anchor
+docker run -it --net chord-20-net --ip $anchorIP chord-20:anchor java src.Anchor
 
--- chord/runClient.sh
+++ chord/runClient.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 uuid=$1
-sudo docker run -it --net chord-20-net chord-20:client java src.Client $uuid
+docker run -it --net chord-20-net chord-20:client java src.Client $uuid
